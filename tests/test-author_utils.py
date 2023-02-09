787e603abebe884039cf218e7c717ad94517e832
Martin Fenner <martin@front-matter.io>
2023-02-09 19:41:46 +0100

    linting

--- tests/test-author_utils.py
+++ tests/test-author_utils.py
@@ -1,116 +1,289 @@
 import pytest
-from talbot.author_utils import cleanup_author, authors_as_string, get_one_author, get_authors, get_affiliations
+from talbot.author_utils import (
+    cleanup_author,
+    authors_as_string,
+    get_one_author,
+    get_authors,
+    get_affiliations,
+)
+
 
 def test_one_authpr():
     "one author"
     authors = [
-        {'ORCID': 'http://orcid.org/0000-0003-0077-4738',
-         'given': 'Matt',
-         'family': 'Jones'}
+        {
+            "ORCID": "http://orcid.org/0000-0003-0077-4738",
+            "given": "Matt",
+            "family": "Jones",
+        }
     ]
-    assert {'familyName': 'Jones', 'givenName': 'Matt', 'name': 'Matt Jones', 'nameType': 'Personal'} == get_one_author(authors[0])
-    # has familyName
-    assert {'nameIdentifiers': [{ 'nameIdentifier': 'https://orcid.org/0000-0003-1419-2405',
-            'nameIdentifierScheme': 'ORCID', 'schemeUri': 'https://orcid.org' }], 
-            'name': 'Martin Fenner', 
-            'givenName': 'Martin',
-            'familyName': 'Fenner',
-            'nameType': 'Personal'} == get_one_author({'name': 'Fenner, Martin', 'givenName': 'Martin', 'familyName': 'Fenner', 'nameIdentifiers': [{'nameIdentifier': 'https://orcid.org/0000-0003-1419-2405', 'nameIdentifierScheme': 'ORCID', 'schemeUri': 'https://orcid.org'}]})
-    # has name in sort-order' do
-    assert {'nameType': 'Personal', 'name': 'Benjamin Ollomo',
-        'givenName': 'Benjamin', 'familyName': 'Ollomo', 'affiliation': [{ 'affiliationIdentifier': 'https://ror.org/01wyqb997', 'affiliationIdentifierScheme': 'ROR', 'name': 'Centre International de Recherches Médicales de Franceville' }]} == get_one_author({'name': 'Ollomo, Benjamin', 'givenName': 'Benjamin', 'familyName': 'Ollomo', 'affiliation': [{'name': 'Centre International de Recherches Médicales de Franceville', 'affiliationIdentifier': '01wyqb997', 'affiliationIdentifierScheme': 'ROR', 'schemeURI': 'https://ror.org/'}]})
-    # has name in Thai
-    assert ({"name": 'กัญจนา แซ่เตียว'}) == get_one_author({"name": "กัญจนา แซ่เตียว","affiliation": [],"nameIdentifiers": []})
-    # multiple author names in one field
-    assert {'name': 'Enos, Ryan (Harvard University); Fowler, Anthony (University of Chicago); Vavreck, Lynn (UCLA)'} == get_one_author({"name": "Enos, Ryan (Harvard University); Fowler, Anthony (University of Chicago); Vavreck, Lynn (UCLA)", "affiliation": [],"nameIdentifiers": []})
-    # 'hyper-authorship'
-    assert {'name': 'ALICE Collaboration'} == get_one_author({"name": "ALICE Collaboration", "nameType": "Organizational", "affiliation": [], "nameIdentifiers": []})
-    # is organization
-    author = {'email': 'info@ucop.edu',
-        'creatorName': { '__content__': 'University of California, Santa Barbara', 'nameType': 'Organizational' }, 'role': { 'namespace': 'http://www.ngdc.noaa.gov/metadata/published/xsd/schema/resources/Codelist/gmxCodelists.xml#CI_RoleCode', 'roleCode': 'copyrightHolder' } }
-    assert {'name': 'University of California, Santa Barbara', "nameType": "Organizational"} == get_one_author(author)
-    # name with affiliation crossref
-    assert {'affiliation': [{"name": "Department of Plant Molecular Biology, University of Lausanne, Lausanne, Switzerland"}],'familyName': 'Sankar',
-        'givenName': 'Martial', 'name': 'Martial Sankar', "nameType": "Personal"} == get_one_author({"given": "Martial", "family": "Sankar", "sequence": "first", "affiliation": [{"name": "Department of Plant Molecular Biology, University of Lausanne, Lausanne, Switzerland"}]})
-    # multiple name_identifier
-    assert {'nameType': 'Personal', 'name': 'Thomas Dubos', 'givenName': 'Thomas', 'familyName': 'Dubos', 
-        'affiliation': [{ 'name': 'École Polytechnique\nLaboratoire de Météorologie Dynamique' }], 
-        'nameIdentifiers': [{ 'nameIdentifier': 'http://isni.org/isni/0000 0003 5752 6882', 'nameIdentifierScheme': 'ISNI' },
-        { 'nameIdentifier': 'https://orcid.org/0000-0003-4514-4211', 'nameIdentifierScheme': 'ORCID', 'schemeUri': 'https://orcid.org' }]} == get_one_author({
-        "name": "Dubos, Thomas",
+    assert {
+        "familyName": "Jones",
+        "givenName": "Matt",
+        "name": "Matt Jones",
         "nameType": "Personal",
+    } == get_one_author(authors[0])
+    # has familyName
+    assert {
+        "nameIdentifiers": [
+            {
+                "nameIdentifier": "https://orcid.org/0000-0003-1419-2405",
+                "nameIdentifierScheme": "ORCID",
+                "schemeUri": "https://orcid.org",
+            }
+        ],
+        "name": "Martin Fenner",
+        "givenName": "Martin",
+        "familyName": "Fenner",
+        "nameType": "Personal",
+    } == get_one_author(
+        {
+            "name": "Fenner, Martin",
+            "givenName": "Martin",
+            "familyName": "Fenner",
+            "nameIdentifiers": [
+                {
+                    "nameIdentifier": "https://orcid.org/0000-0003-1419-2405",
+                    "nameIdentifierScheme": "ORCID",
+                    "schemeUri": "https://orcid.org",
+                }
+            ],
+        }
+    )
+    # has name in sort-order' do
+    assert {
+        "nameType": "Personal",
+        "name": "Benjamin Ollomo",
+        "givenName": "Benjamin",
+        "familyName": "Ollomo",
+        "affiliation": [
+            {
+                "affiliationIdentifier": "https://ror.org/01wyqb997",
+                "affiliationIdentifierScheme": "ROR",
+                "name": "Centre International de Recherches Médicales de Franceville",
+            }
+        ],
+    } == get_one_author(
+        {
+            "name": "Ollomo, Benjamin",
+            "givenName": "Benjamin",
+            "familyName": "Ollomo",
+            "affiliation": [
+                {
+                    "name": "Centre International de Recherches Médicales de Franceville",
+                    "affiliationIdentifier": "01wyqb997",
+                    "affiliationIdentifierScheme": "ROR",
+                    "schemeURI": "https://ror.org/",
+                }
+            ],
+        }
+    )
+    # has name in Thai
+    assert ({"name": "กัญจนา แซ่เตียว"}) == get_one_author(
+        {"name": "กัญจนา แซ่เตียว", "affiliation": [], "nameIdentifiers": []}
+    )
+    # multiple author names in one field
+    assert {
+        "name": "Enos, Ryan (Harvard University); Fowler, Anthony (University of Chicago); Vavreck, Lynn (UCLA)"
+    } == get_one_author(
+        {
+            "name": "Enos, Ryan (Harvard University); Fowler, Anthony (University of Chicago); Vavreck, Lynn (UCLA)",
+            "affiliation": [],
+            "nameIdentifiers": [],
+        }
+    )
+    # 'hyper-authorship'
+    assert {"name": "ALICE Collaboration"} == get_one_author(
+        {
+            "name": "ALICE Collaboration",
+            "nameType": "Organizational",
+            "affiliation": [],
+            "nameIdentifiers": [],
+        }
+    )
+    # is organization
+    author = {
+        "email": "info@ucop.edu",
+        "creatorName": {
+            "__content__": "University of California, Santa Barbara",
+            "nameType": "Organizational",
+        },
+        "role": {
+            "namespace": "http://www.ngdc.noaa.gov/metadata/published/xsd/schema/resources/Codelist/gmxCodelists.xml#CI_RoleCode",
+            "roleCode": "copyrightHolder",
+        },
+    }
+    assert {
+        "name": "University of California, Santa Barbara",
+        "nameType": "Organizational",
+    } == get_one_author(author)
+    # name with affiliation crossref
+    assert {
+        "affiliation": [
+            {
+                "name": "Department of Plant Molecular Biology, University of Lausanne, Lausanne, Switzerland"
+            }
+        ],
+        "familyName": "Sankar",
+        "givenName": "Martial",
+        "name": "Martial Sankar",
+        "nameType": "Personal",
+    } == get_one_author(
+        {
+            "given": "Martial",
+            "family": "Sankar",
+            "sequence": "first",
+            "affiliation": [
+                {
+                    "name": "Department of Plant Molecular Biology, University of Lausanne, Lausanne, Switzerland"
+                }
+            ],
+        }
+    )
+    # multiple name_identifier
+    assert {
+        "nameType": "Personal",
+        "name": "Thomas Dubos",
         "givenName": "Thomas",
         "familyName": "Dubos",
         "affiliation": [
-            "École Polytechnique\nLaboratoire de Météorologie Dynamique"
+            {"name": "École Polytechnique\nLaboratoire de Météorologie Dynamique"}
         ],
         "nameIdentifiers": [
             {
-                 "nameIdentifier": "http://isni.org/isni/0000 0003 5752 6882",
-                 "nameIdentifierScheme": "ISNI"
+                "nameIdentifier": "http://isni.org/isni/0000 0003 5752 6882",
+                "nameIdentifierScheme": "ISNI",
             },
             {
-                 "nameIdentifier": "https://orcid.org/0000-0003-4514-4211",
-                 "nameIdentifierScheme": "ORCID"
-            }
-        ]
-    })
+                "nameIdentifier": "https://orcid.org/0000-0003-4514-4211",
+                "nameIdentifierScheme": "ORCID",
+                "schemeUri": "https://orcid.org",
+            },
+        ],
+    } == get_one_author(
+        {
+            "name": "Dubos, Thomas",
+            "nameType": "Personal",
+            "givenName": "Thomas",
+            "familyName": "Dubos",
+            "affiliation": [
+                "École Polytechnique\nLaboratoire de Météorologie Dynamique"
+            ],
+            "nameIdentifiers": [
+                {
+                    "nameIdentifier": "http://isni.org/isni/0000 0003 5752 6882",
+                    "nameIdentifierScheme": "ISNI",
+                },
+                {
+                    "nameIdentifier": "https://orcid.org/0000-0003-4514-4211",
+                    "nameIdentifierScheme": "ORCID",
+                },
+            ],
+        }
+    )
     # only familyName and givenName
-    assert {'nameType': 'Personal', 'name': 'Emma Johansson',
-        'givenName': 'Emma', 'familyName': 'Johansson'} == get_one_author({'givenName': 'Emma', 'familyName': 'Johansson', "affiliation": [], "nameIdentifiers": []})
- 
+    assert {
+        "nameType": "Personal",
+        "name": "Emma Johansson",
+        "givenName": "Emma",
+        "familyName": "Johansson",
+    } == get_one_author(
+        {
+            "givenName": "Emma",
+            "familyName": "Johansson",
+            "affiliation": [],
+            "nameIdentifiers": [],
+        }
+    )
+
+
 def test_cleanup_author():
     "cleanup_author"
     assert "John Smith" == cleanup_author("John Smith")
     assert "Smith, John" == cleanup_author("Smith, John")
     assert "Smith, J." == cleanup_author("Smith, J.")
 
+
 def test_authors_as_string():
     "authors_as_string"
     authors = [
-        {'nameType': 'Person',
-         'id': 'http://orcid.org/0000-0003-0077-4738',
-         'givenName': 'Matt',
-         'familyName': 'Jones'},
-        {'nameType': 'Person',
-         'id': 'http://orcid.org/0000-0002-2192-403X',
-         'givenName': 'Peter',
-         'familyName': 'Slaughter'},
-        {'nameType': 'Organization',
-         'name': 'University of California, Santa Barbara'}
+        {
+            "nameType": "Person",
+            "id": "http://orcid.org/0000-0003-0077-4738",
+            "givenName": "Matt",
+            "familyName": "Jones",
+        },
+        {
+            "nameType": "Person",
+            "id": "http://orcid.org/0000-0002-2192-403X",
+            "givenName": "Peter",
+            "familyName": "Slaughter",
+        },
+        {"nameType": "Organization", "name": "University of California, Santa Barbara"},
     ]
-    assert "Jones, Matt and Slaughter, Peter" == authors_as_string(authors[0:2])
+    assert "Jones, Matt and Slaughter, Peter" == authors_as_string(
+        authors[0:2])
     # single author
     assert "Jones, Matt" == authors_as_string(authors[0])
     # no authors
     assert None is authors_as_string(None)
     # with organization
-    assert "Jones, Matt and Slaughter, Peter and University of California, Santa Barbara" == authors_as_string(authors)
+    assert (
+        "Jones, Matt and Slaughter, Peter and University of California, Santa Barbara"
+        == authors_as_string(authors)
+    )
+
 
 def test_get_authors():
     "get_authors"
     authors = [
-        {'ORCID': 'https://orcid.org/0000-0003-0077-4738',
-         'given': 'Matt',
-         'family': 'Jones'},
-        {'given': 'Peter',
-         'family': 'Slaughter'},
-        {'name': 'University of California, Santa Barbara'}
+        {
+            "ORCID": "https://orcid.org/0000-0003-0077-4738",
+            "given": "Matt",
+            "family": "Jones",
+        },
+        {"given": "Peter", "family": "Slaughter"},
+        {"name": "University of California, Santa Barbara"},
     ]
-    assert [{'nameType': 'Personal',
-             'givenName': 'Matt',
-             'familyName': 'Jones',
-             'name': 'Matt Jones'},
-            {'nameType': 'Personal',
-             'name': 'Peter Slaughter',
-             'givenName': 'Peter', 
-             'familyName': 'Slaughter'},
-            {'name': 'University of California, Santa Barbara'}] == get_authors(authors)
+    assert [
+        {
+            "nameType": "Personal",
+            "givenName": "Matt",
+            "familyName": "Jones",
+            "name": "Matt Jones",
+        },
+        {
+            "nameType": "Personal",
+            "name": "Peter Slaughter",
+            "givenName": "Peter",
+            "familyName": "Slaughter",
+        },
+        {"name": "University of California, Santa Barbara"},
+    ] == get_authors(authors)
+
 
 def test_get_affiliations():
     "get_affiliations"
     assert None == get_affiliations(None)
-    assert [{'name': 'University of California, Santa Barbara'}] == get_affiliations(["University of California, Santa Barbara"])
-    assert [{'name': 'University of California, Santa Barbara'}] == get_affiliations([{'name': 'University of California, Santa Barbara'}])
-    assert [{'name': 'University of California, Santa Barbara', 'affiliationIdentifier': 'https://ror.org/02t274463', 'affiliationIdentifierScheme': 'ROR'}] == get_affiliations([{'name': 'University of California, Santa Barbara', 'affiliationIdentifier': '02t274463', 'affiliationIdentifierScheme': 'ROR', 'schemeURI': 'https://ror.org/'}])
-    
+    assert [{"name": "University of California, Santa Barbara"}] == get_affiliations(
+        ["University of California, Santa Barbara"]
+    )
+    assert [{"name": "University of California, Santa Barbara"}] == get_affiliations(
+        [{"name": "University of California, Santa Barbara"}]
+    )
+    assert [
+        {
+            "name": "University of California, Santa Barbara",
+            "affiliationIdentifier": "https://ror.org/02t274463",
+            "affiliationIdentifierScheme": "ROR",
+        }
+    ] == get_affiliations(
+        [
+            {
+                "name": "University of California, Santa Barbara",
+                "affiliationIdentifier": "02t274463",
+                "affiliationIdentifierScheme": "ROR",
+                "schemeURI": "https://ror.org/",
+            }
+        ]
+    )
--- tests/test-bibtex-writer.py
+++ tests/test-bibtex-writer.py
@@ -8,12 +8,13 @@ def test_doi_with_data_citation():
     "DOi with data citation"
     subject = Metadata("10.7554/elife.01567")
     assert subject.pid == "https://doi.org/10.7554/elife.01567"
-    assert subject.types.get('bibtex') == 'article'
+    assert subject.types.get("bibtex") == "article"
 
     bibtex = subject.bibtex()
     print(bibtex)
-    assert bibtex == \
-"""@article{https://doi.org/10.7554/elife.01567,
+    assert (
+        bibtex
+        == """@article{https://doi.org/10.7554/elife.01567,
     abstract = {Among various advantages, their small size makes model organisms preferred subjects of investigation. Yet, even in model systems detailed analysis of numerous developmental processes at cellular level is severely hampered by their scale. For instance, secondary growth of Arabidopsis hypocotyls creates a radial pattern of highly specialized tissues that comprises several thousand cells starting from a few dozen. This dynamic process is difficult to follow because of its scale and because it can only be investigated invasively, precluding comprehensive understanding of the cell proliferation, differentiation, and patterning events involved. To overcome such limitation, we established an automated quantitative histology approach. We acquired hypocotyl cross-sections from tiled high-resolution images and extracted their information content using custom high-throughput image processing and segmentation. Coupled with automated cell type recognition through machine learning, we could establish a cellular resolution atlas that reveals vascular morphodynamics during secondary growth, for example equidistant phloem pole formation.},
     author = {Sankar, Martial and Nieminen, Kaisa and Ragni, Laura and Xenarios, Ioannis and Hardtke, Christian S},
     copyright = {https://creativecommons.org/licenses/by/3.0/legalcode},
@@ -28,18 +29,20 @@ def test_doi_with_data_citation():
     year = {2014}
 }
 """
+    )
 
 
 def test_doi_for_blog_post():
     "DOi for blog post"
     subject = Metadata("10.53731/avg2ykg-gdxppcd")
     assert subject.pid == "https://doi.org/10.53731/avg2ykg-gdxppcd"
-    assert subject.types.get('bibtex') == 'article'
+    assert subject.types.get("bibtex") == "article"
 
     bibtex = subject.bibtex()
     print(bibtex)
-    assert bibtex == \
-"""@article{https://doi.org/10.53731/avg2ykg-gdxppcd,
+    assert (
+        bibtex
+        == """@article{https://doi.org/10.53731/avg2ykg-gdxppcd,
     abstract = {Science blogs have been around for at least 20 years and have become an important part of science communication. So are there any fundamental issues that need fixing? Barriers to Entry Blogging platforms are mature at this point, and the technology is not imposing barriers to entry for most people. The user experience has greatly improved over the last few years and there are a number of affordable ways for hosting a blog that also work for science blogs, including free options such as GitHub},
     author = {Fenner, Martin},
     copyright = {https://creativecommons.org/licenses/by/4.0/legalcode},
@@ -51,17 +54,20 @@ def test_doi_for_blog_post():
     year = {2023}
 }
 """
+    )
+
 
 def test_blog_post():
     "blog post"
     input = "https://upstream.force11.org/welcome-to-upstream/"
-    subject = Metadata(input, via='schema_org')
+    subject = Metadata(input, via="schema_org")
     assert subject.pid == "https://doi.org/10.54900/rckn8ey-1fm76va-qsrnf"
-    assert subject.types.get('bibtex') == 'article'
+    assert subject.types.get("bibtex") == "article"
     bibtex = subject.bibtex()
     print(bibtex)
-    assert bibtex == \
-"""@article{https://doi.org/10.54900/rckn8ey-1fm76va-qsrnf,
+    assert (
+        bibtex
+        == """@article{https://doi.org/10.54900/rckn8ey-1fm76va-qsrnf,
     abstract = {Today we are announcing Upstream. And if you’re reading this, you’re already a part of it! Upstream is a community blogging platform designed for Open enthusiasts to discuss… you guessed it: all things Open. It’s a space for the whole community to voice opinions, discuss open approaches to scholarly communication, and showcase research. A central place to exchange in writing ideas about open research and all that it encompasses Supported by FORCE11, this is a global and inclusive blog, bringi},
     author = {Chodacki, John and Hendricks, Ginny and Ferguson, Christine and Fenner, Martin},
     copyright = {https://creativecommons.org/licenses/by/4.0/legalcode},
@@ -75,18 +81,21 @@ def test_blog_post():
     year = {2021}
 }
 """
+    )
+
 
 def test_article_with_pages():
     "article with pages"
     input = "https://doi.org/10.1371/journal.ppat.1008184"
     subject = Metadata(input)
     assert subject.pid == "https://doi.org/10.1371/journal.ppat.1008184"
-    assert subject.types.get('bibtex') == 'article'
+    assert subject.types.get("bibtex") == "article"
 
     bibtex = subject.bibtex()
     print(bibtex)
-    assert bibtex == \
-"""@article{https://doi.org/10.1371/journal.ppat.1008184,
+    assert (
+        bibtex
+        == """@article{https://doi.org/10.1371/journal.ppat.1008184,
     author = {Twittenhoff, Christian and Heroven, Ann Kathrin and Mühlen, Sabrina and Dersch, Petra and Narberhaus, Franz},
     copyright = {https://creativecommons.org/licenses/by/4.0/legalcode},
     doi = {10.1371/journal.ppat.1008184},
@@ -102,18 +111,21 @@ def test_article_with_pages():
     year = {2020}
 }
 """
+    )
+
 
 def test_article_dlib_magazine():
     "article dlib magazine"
     input = "https://doi.org/10.1045/january2017-burton"
     subject = Metadata(input)
     assert subject.pid == "https://doi.org/10.1045/january2017-burton"
-    assert subject.types.get('bibtex') == 'article'
+    assert subject.types.get("bibtex") == "article"
 
     bibtex = subject.bibtex()
     print(bibtex)
-    assert bibtex == \
-"""@article{https://doi.org/10.1045/january2017-burton,
+    assert (
+        bibtex
+        == """@article{https://doi.org/10.1045/january2017-burton,
     author = {Burton, Adrian and Aryani, Amir and Koers, Hylke and Manghi, Paolo and La Bruzzo, Sandro and Stocker, Markus and Diepenbroek, Michael and Schindler, Uwe and Fenner, Martin},
     doi = {10.1045/january2017-burton},
     issn = {1082-9873},
@@ -127,3 +139,4 @@ def test_article_dlib_magazine():
     year = {2017}
 }
 """
+    )
