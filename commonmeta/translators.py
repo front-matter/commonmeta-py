56002aa79f9e8c82efd14e028dc8ff9fc54661ab
Martin Fenner <martin@front-matter.io>
2024-03-15 10:02:35 +0100

    add translator for datacite blog

--- commonmeta/translators.py
+++ commonmeta/translators.py
@@ -8,12 +8,21 @@ def web_translator(soup, url: str):
     f = furl(url)
     if f.host == "arxiv.org":
         return arxiv_translator(soup)
+    elif f.host == "datacite.org":
+        return datacite_translator(soup)
     return {}
 
 
 def arxiv_translator(soup):
-    """Extract metadata from arXiv. Find the DOI and lookup the metadata via DataCite."""
+    """Extract metadata from arXiv. Find the DOI and return it."""
     arxiv_id = soup.select_one("meta[name='citation_arxiv_id']")
     if arxiv_id is None:
-        return None
+        return {}
     return {"@id": f"https://doi.org/10.48550/arXiv.{arxiv_id['content']}"}
+  
+def datacite_translator(soup):
+    """Extract metadata from DataCite blog posts. Find the DOI and return it."""
+    doi = soup.select_one("div#citation")
+    if doi is None:
+        return {}
+    return {"@id": doi.get("data-doi", None)}
--- tests/test-translators.py
+++ tests/test-translators.py
@@ -8,6 +8,7 @@ import pytest  # noqa: F401
 from commonmeta.translators import web_translator
 from bs4 import BeautifulSoup
 
+
 def test_web_translator_arxiv():
     """Test web translator for arXiv"""
     filepath = path.join(path.dirname(__file__), "fixtures", "arxiv.html")
@@ -16,3 +17,13 @@ def test_web_translator_arxiv():
     soup = BeautifulSoup(string, "html.parser")
     metadata = web_translator(soup, "https://arxiv.org/abs/1902.02534")
     assert re.match(r"https://doi.org/10.48550/arXiv.1902.02534", metadata["@id"])
+
+
+def test_web_translator_datacite():
+    """Test web translator for DataCite"""
+    filepath = path.join(path.dirname(__file__), "fixtures", "datacite.html")
+    with open(filepath, encoding="utf-8") as file:
+        string = file.read()
+    soup = BeautifulSoup(string, "html.parser")
+    metadata = web_translator(soup, "https://datacite.org/blog/bioschemas_2024/")
+    assert re.match(r"https://doi.org/10.5438/vzqp-m504", metadata["@id"])
