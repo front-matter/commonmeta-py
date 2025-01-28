ee10e21d7ff57bd0dfb76673802511bd16c69996
Martin Fenner <martin@front-matter.io>
2025-01-28 21:53:53 +0100

    version bump

--- commonmeta/__init__.py
+++ commonmeta/__init__.py
@@ -10,7 +10,7 @@ commonmeta-py is a Python library to convert scholarly metadata
 """
 
 __title__ = "commonmeta-py"
-__version__ = "0.70"
+__version__ = "0.72"
 __author__ = "Martin Fenner"
 __license__ = "MIT"
 
