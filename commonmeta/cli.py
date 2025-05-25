41c220cc46ee63765867313b794b4cfb7c9057a7
Martin Fenner <martin@front-matter.io>
2025-05-25 16:06:12 +0200

    add put and push commands to cli

--- commonmeta/cli.py
+++ commonmeta/cli.py
@@ -66,6 +66,49 @@ def convert(
 
 
 @cli.command()
+@click.argument("input", type=str, required=True)
+@click.option("--via", "-f", type=str, default=None)
+@click.option("--to", "-t", type=str, default="commonmeta")
+@click.option("--style", "-s", type=str, default="apa")
+@click.option("--locale", "-l", type=str, default="en-US")
+@click.option("--doi", type=str)
+@click.option("--prefix", type=str)
+@click.option("--depositor", type=str)
+@click.option("--email", type=str)
+@click.option("--registrant", type=str)
+@click.option("--show-errors/--no-errors", type=bool, show_default=True, default=False)
+def put(
+    input,
+    via,
+    to,
+    style,
+    locale,
+    doi,
+    prefix,
+    depositor,
+    email,
+    registrant,
+    show_errors,
+):
+    metadata = Metadata(input, via=via, doi=doi, prefix=prefix)
+    if show_errors and not metadata.is_valid:
+        raise click.ClickException(str(metadata.errors) + str(metadata.write_errors))
+
+    click.echo(
+        metadata.write(
+            to=to,
+            style=style,
+            locale=locale,
+            depositor=depositor,
+            email=email,
+            registrant=registrant,
+        )
+    )
+    if show_errors and metadata.write_errors:
+        raise click.ClickException(str(metadata.write_errors))
+
+
+@cli.command()
 @click.argument("string", type=str, required=True)
 @click.option("--via", "-f", type=str)
 @click.option("--to", "-t", type=str, default="commonmeta")
@@ -80,6 +123,57 @@ def convert(
 @click.option("--show-errors/--no-errors", type=bool, show_default=True, default=False)
 @click.option("--show-timer/--no-timer", type=bool, show_default=True, default=False)
 def list(
+    string,
+    via,
+    to,
+    style,
+    locale,
+    prefix,
+    depositor,
+    email,
+    registrant,
+    filename,
+    jsonlines,
+    show_errors,
+    show_timer,
+):
+    start = time.time()
+    metadata_list = MetadataList(
+        string,
+        via=via,
+        depositor=depositor,
+        email=email,
+        registrant=registrant,
+        prefix=prefix,
+        filename=filename,
+        jsonlines=jsonlines,
+    )
+    end = time.time()
+    runtime = end - start
+    if show_errors and not metadata_list.is_valid:
+        raise click.ClickException(str(metadata_list.errors))
+    click.echo(metadata_list.write(to=to, style=style, locale=locale))
+    if show_errors and len(metadata_list.write_errors) > 0:
+        raise click.ClickException(str(metadata_list.write_errors))
+    if show_timer:
+        click.echo(f"Runtime: {runtime:.2f} seconds")
+
+
+@cli.command()
+@click.argument("string", type=str, required=True)
+@click.option("--via", "-f", type=str)
+@click.option("--to", "-t", type=str, default="commonmeta")
+@click.option("--style", "-s", type=str, default="apa")
+@click.option("--locale", "-l", type=str, default="en-US")
+@click.option("--prefix", type=str)
+@click.option("--depositor", type=str)
+@click.option("--email", type=str)
+@click.option("--registrant", type=str)
+@click.option("--filename", type=str)
+@click.option("--jsonlines/--no-jsonlines", type=bool, show_default=True, default=False)
+@click.option("--show-errors/--no-errors", type=bool, show_default=True, default=False)
+@click.option("--show-timer/--no-timer", type=bool, show_default=True, default=False)
+def push(
     string,
     via,
     to,
