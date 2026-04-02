4b8e5e5ab239a28d5ac604d55d219f9ecc681495
Martin Fenner <martin@front-matter.de>
2026-04-02 18:04:44 +0200

    require Python 3.10

--- commonmeta/__init__.py
+++ commonmeta/__init__.py
@@ -10,7 +10,7 @@ commonmeta-py is a Python library to convert scholarly metadata
 """
 
 __title__ = "commonmeta-py"
-__version__ = "0.209"
+__version__ = "0.211"
 __author__ = "Martin Fenner"
 __license__ = "MIT"
 
