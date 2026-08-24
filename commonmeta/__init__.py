6043bfcd1dda667024c9f64f9d175eb110ccf794
Martin Fenner <martin@front-matter.de>
2026-08-24 16:26:56 +0200

    chore: bump to 0.278

--- commonmeta/__init__.py
+++ commonmeta/__init__.py
@@ -10,7 +10,7 @@ commonmeta-py is a Python library to convert scholarly metadata
 """
 
 __title__ = "commonmeta-py"
-__version__ = "0.277"
+__version__ = "0.278"
 __author__ = "Martin Fenner"
 __license__ = "MIT"
 
